62b8f5d68be49fbd878bae8daf0e7a54d53a12b8
Doodles <moraes.eduardo@proton.me>
2023-04-19 22:48:18 -0300

    chore: style the account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -20,42 +20,50 @@
 	}
 </script>
 
-<div class="form-widget">
-	<form
-		class="form-widget"
-		method="post"
-		action="?/update"
-		use:enhance={handleSubmit}
-		bind:this={profileForm}
-	>
-		<div>
-			<label for="email">Email</label>
-			<input id="email" type="text" value={session.user.email} disabled />
-		</div>
+<form
+	method="post"
+	action="?/update"
+	use:enhance={handleSubmit}
+	bind:this={profileForm}
+	class="mb-3"
+>
+	<div class="mb-3">
+		<label for="email" class="form-label">Email</label>
+		<input id="email" type="text" value={session.user.email} class="form-control" disabled />
+	</div>
 
-		<div>
-			<label for="fullName">Full Name</label>
-			<input id="fullName" name="fullName" type="text" value={form?.fullName ?? fullName} />
-		</div>
+	<div class="mb-3">
+		<label for="fullName" class="form-label">Full Name</label>
+		<input
+			id="fullName"
+			name="fullName"
+			type="text"
+			value={form?.fullName ?? fullName}
+			class="form-control"
+		/>
+	</div>
 
-		<div>
-			<label for="username">Username</label>
-			<input id="username" name="username" type="text" value={form?.username ?? username} />
-		</div>
+	<div class="mb-3">
+		<label for="username" class="form-label">Username</label>
+		<input
+			id="username"
+			name="username"
+			type="text"
+			value={form?.username ?? username}
+			class="form-control"
+		/>
+	</div>
 
-		<div>
-			<input
-				type="submit"
-				class="button block primary"
-				value={loading ? 'Loading...' : 'Update'}
-				disabled={loading}
-			/>
-		</div>
-	</form>
+	<div>
+		<input
+			type="submit"
+			class="btn btn-primary"
+			value={loading ? 'Loading...' : 'Update'}
+			disabled={loading}
+		/>
+	</div>
+</form>
 
-	<form method="post" action="?/signout" use:enhance={handleSubmit}>
-		<div>
-			<button class="button block" disabled={loading}>Sign Out</button>
-		</div>
-	</form>
-</div>
+<form method="post" action="?/signout" use:enhance={handleSubmit}>
+	<button class="btn btn-danger" disabled={loading}>Sign Out</button>
+</form>
