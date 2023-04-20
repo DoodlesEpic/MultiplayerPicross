112fdb74fb91a3a64d2095649568981f56fc6678
Doodles <moraes.eduardo@proton.me>
2023-04-19 22:13:37 -0300

    feat: add room list page

---
+++ src/routes/rooms/+page.server.ts
@@ -0,0 +1,14 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ locals: { supabase, getSession } }) => {
+	const session = await getSession();
+
+	if (!session) {
+		throw redirect(303, '/');
+	}
+
+	const { data: rooms } = await supabase.from('rooms').select(`created_at, creator, players`);
+
+	return { session, rooms };
+}) satisfies PageServerLoad;
---
+++ src/routes/rooms/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let { session, rooms } = data;
+	let roomlist: any[] = rooms ?? [];
+	let roomlistlength = roomlist?.length ?? 0;
+
+	console.log(roomlist);
+	console.log(roomlistlength);
+</script>
+
+<div>
+	<h1>Rooms</h1>
+
+	{#if roomlistlength > 0}
+		<ul>
+			{#each roomlist as room}
+				<li>
+					<div>
+						<h2>{room.creator}</h2>
+						<p>{room.created_at}</p>
+						<p>{room.players?.length ?? 0}</p>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>No rooms</p>
+	{/if}
+
+	<button disabled>Create room</button>
+</div>
