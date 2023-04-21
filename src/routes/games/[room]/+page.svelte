8dc7397a7bcf44193d2f210d3cff9dc3cf588f01
Doodles <moraes.eduardo@proton.me>
2023-04-20 22:06:12 -0300

    feat: implement game data synchronization

--- src/routes/games/[room]/+page.svelte
+++ src/routes/games/[room]/+page.svelte
@@ -5,8 +5,33 @@
 	export let data: PageData;
 	let { session, room } = data;
 
-	const handleClick = (tile: boolean, position: number) => {
+	$page.data.supabase
+		.channel('db-messages')
+		.on(
+			'postgres_changes',
+			{
+				event: 'UPDATE',
+				schema: 'public',
+				table: 'rooms',
+				filter: `id=eq.${room?.id}`
+			},
+			(payload: { new: any }) => (room = payload.new as any)
+		)
+		.subscribe(async (status: string) => {
+			if (status === 'SUBSCRIBED') {
+				const res = await $page.data.supabase.from('rooms').select().eq('id', room?.id).single();
+				console.log('Res: ', res);
+			}
+		});
+
+	const handleClick = async (tile: boolean, position: number) => {
 		room.current[position] = !tile;
+		const { error } = await $page.data.supabase
+			.from('rooms')
+			.update({ current: room?.current })
+			.eq('id', room?.id);
+		if (error) console.log(error);
+		console.log('Updated');
 	};
 </script>
 
@@ -14,7 +39,7 @@
 <!-- <p>{JSON.stringify(room, null, 2)}</p> -->
 
 <div>
-	{#each room.current as tile, position}
+	{#each room?.current as tile, position}
 		<button
 			on:click={() => handleClick(tile, position)}
 			class={`btn p-5 mb-1 ${tile ? 'btn-primary' : 'btn-danger'}`}
