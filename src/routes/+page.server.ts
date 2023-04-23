0a12925c46eb29f0a8df2582606d4fcecc5157b2
Doodles <moraes.eduardo@proton.me>
2023-04-23 17:02:44 -0300

    feat: redirect player to room list when logged in

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,11 +3,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
-
-	// if the user is already logged in return them to the account page
-	if (session) {
-		throw redirect(303, '/account');
-	}
+	if (session) throw redirect(303, '/rooms');
 
 	return { url: url.origin };
 };
