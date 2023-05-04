b38beb7c234d663f0841891b7ebe295d35e676a7
Doodles <moraes.eduardo@proton.me>
2023-05-04 20:42:55 -0300

    feat: implement universal loading on profile

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,21 +1,5 @@
 import { fail, redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
-
-export const load = (async ({ locals: { supabase, getSession } }) => {
-	const session = await getSession();
-
-	if (!session) {
-		throw redirect(303, '/');
-	}
-
-	const { data: profile } = await supabase
-		.from('profiles')
-		.select(`username, full_name, avatar_url`)
-		.eq('id', session.user.id)
-		.single();
-
-	return { session, profile };
-}) satisfies PageServerLoad;
+import type { Actions } from './$types';
 
 export const actions = {
 	update: async ({ request, locals: { supabase, getSession } }) => {
---
+++ src/routes/account/+page.ts
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load = (async ({ parent }) => {
+	const { session, supabase } = await parent();
+	if (!session) throw redirect(303, '/');
+
+	const { data: profile } = await supabase
+		.from('profiles')
+		.select(`username, full_name, avatar_url`)
+		.eq('id', session.user.id)
+		.single();
+
+	return { profile };
+}) satisfies PageLoad;
