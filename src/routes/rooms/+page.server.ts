45c633f16b2fb39829025e38d1f42fd60b824390
Doodles <moraes.eduardo@proton.me>
2023-05-04 19:37:11 -0300

    chore: show more recent unsolved rooms first

--- src/routes/rooms/+page.server.ts
+++ src/routes/rooms/+page.server.ts
@@ -5,6 +5,12 @@ export const load = (async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
 	if (!session) throw redirect(303, '/');
 
-	const { data: rooms } = await supabase.from('rooms').select(`id, created_at, creator, solved`);
+	const { data: rooms, error } = await supabase
+		.from('rooms')
+		.select(`id, created_at, creator, solved`)
+		.order('solved', { ascending: true })
+		.order('created_at', { ascending: false });
+	if (error) throw error;
+
 	return { session, rooms };
 }) satisfies PageServerLoad;
