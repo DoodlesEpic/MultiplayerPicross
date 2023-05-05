8d6303e3cf6e91ef6e90a31a6db372def4cd8f3b
Doodles <moraes.eduardo@proton.me>
2023-05-04 21:48:24 -0300

    fix: account update action using undefined local helper

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -2,13 +2,14 @@ import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions = {
-	update: async ({ request, locals: { supabase, getSession } }) => {
+	update: async ({ request, locals: { supabase } }) => {
+		const session = (await supabase.auth.getSession()).data.session;
+		if (!session) throw redirect(303, '/');
+
 		const formData = await request.formData();
 		const fullName = formData.get('fullName') as string;
 		const username = formData.get('username') as string;
 		const avatarUrl = formData.get('avatarUrl') as string;
-
-		const session = await getSession();
 
 		const { error } = await supabase.from('profiles').upsert({
 			id: session?.user.id,
