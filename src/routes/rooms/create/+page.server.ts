d381cd3c478080119757b215610d7a6f159bb72d
Doodles <moraes.eduardo@proton.me>
2023-05-04 21:43:48 -0300

    fix: wrong type import on create room server.ts

--- src/routes/rooms/create/+page.server.ts
+++ src/routes/rooms/create/+page.server.ts
@@ -1,5 +1,5 @@
 import { fail, redirect } from '@sveltejs/kit';
-import type { Actions } from '../$types';
+import type { Actions } from './$types';
 
 export const actions = {
 	create: async ({ request, locals: { supabase, getSession } }) => {
