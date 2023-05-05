30630876eae1966810f2d3c16190aee2a7a16039
Doodles <moraes.eduardo@proton.me>
2023-05-04 21:54:06 -0300

    fix: main parge.server.ts using undefined locals helper

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
-	const session = await getSession();
+export const load: PageServerLoad = async ({ url, locals: { supabase } }) => {
+	const session = (await supabase.auth.getSession()).data.session;
 	if (session) throw redirect(303, '/rooms');
 
 	return { url: url.origin };
