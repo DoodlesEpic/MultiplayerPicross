a3fa6d66438f7a817cb43a7b6ead91498c80fd05
Doodles <moraes.eduardo@proton.me>
2023-05-04 21:41:58 -0300

    chore: throw errors whenever queries fail

--- src/routes/account/+page.ts
+++ src/routes/account/+page.ts
@@ -5,11 +5,12 @@ export const load = (async ({ parent }) => {
 	const { session, supabase } = await parent();
 	if (!session) throw redirect(303, '/');
 
-	const { data: profile } = await supabase
+	const { data: profile, error } = await supabase
 		.from('profiles')
 		.select(`username, full_name, avatar_url`)
 		.eq('id', session.user.id)
 		.single();
+	if (error) throw error;
 
 	return { profile };
 }) satisfies PageLoad;
--- src/routes/games/[room]/+page.ts
+++ src/routes/games/[room]/+page.ts
@@ -5,11 +5,12 @@ export const load = (async ({ parent, params }) => {
 	const { session, supabase } = await parent();
 	if (!session) throw redirect(303, '/');
 
-	const { data: room } = await supabase
+	const { data: room, error } = await supabase
 		.from('rooms')
 		.select(`id, current, creator, created_at, solved, figure ( figure, width )`)
 		.eq('id', params.room)
 		.single();
+	if (error) throw error;
 
 	return { room };
 }) satisfies PageLoad;
