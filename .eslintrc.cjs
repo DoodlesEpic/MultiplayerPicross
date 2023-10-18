f4515d16e6c7f012221c9e8e2baa3245a3bbea80
Doodles <moraes.eduardo@proton.me>
2023-10-18 00:42:37 -0300

    chore: enable and fix new svelte linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,14 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:svelte/recommended',
+		'plugin:svelte/prettier',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
