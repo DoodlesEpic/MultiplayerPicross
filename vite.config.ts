f86c7566aac1c4469b6c9385dad571d8b874ac1f
Doodles <moraes.eduardo@proton.me>
2026-01-09 10:32:13 -0300

    fix: silence sass deprecation warnings
    This is currently necessary because of Bootstrap. For future
    reference, this is being tracked in the following issue:
    https://github.com/twbs/bootstrap/issues/40962

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
+				// silenceDeprecations is currently needed because of a problem with Bootstrap and Dart CSS
+				// See: https://getbootstrap.com/docs/5.3/customize/sass/ (warning at the top of the page)
+				// See: https://github.com/twbs/bootstrap/issues/40962
+				// Whenever Bootstrap fixes it, we can unsilence these warnings
+				silenceDeprecations: [
+					'color-functions',
+					'global-builtin',
+					'import',
+					'if-function',
+					'legacy-js-api'
+				],
 				additionalData: '@use "src/variables.scss" as *;'
 			}
 		}
